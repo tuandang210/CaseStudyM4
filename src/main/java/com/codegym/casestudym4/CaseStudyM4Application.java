f5b9c09496d1b24e5aae5b555edfdc138fb672d0
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-07-04 08:49:43 +0700

    first commit

---
+++ src/main/java/com/codegym/casestudym4/CaseStudyM4Application.java
@@ -0,0 +1,13 @@
+package com.codegym.casestudym4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyM4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyM4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/casestudym4/CaseStudyM4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.casestudym4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyM4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
