c798cc60dd6cc96b4137c725fa454fb67dbb1ca0
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-07-05 11:31:49 +0700

    upload image

--- src/main/java/com/codegym/casestudym4/model/dtoimage/ImageDTO.java
+++ src/main/java/com/codegym/casestudym4/model/dtoimage/ImageDTO.java
@@ -1,4 +1,25 @@
 package com.codegym.casestudym4.model.dtoimage;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class ImageDTO {
+    private String extraField;
+
+    private MultipartFile[] files;
+
+    public String getExtraField() {
+        return extraField;
+    }
+
+    public void setExtraField(String extraField) {
+        this.extraField = extraField;
+    }
+
+    public MultipartFile[] getFiles() {
+        return files;
+    }
+
+    public void setFiles(MultipartFile[] files) {
+        this.files = files;
+    }
 }
