8a79c6cb3cc2bfa2cf5aeffb7a35fca2d1c54b42
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-07-05 08:07:11 +0700

    t

---
+++ src/main/java/com/codegym/casestudym4/model/dtoimage/ImageDTO.java
@@ -0,0 +1,4 @@
+package com.codegym.casestudym4.model.dtoimage;
+
+public class ImageDTO {
+}
--- src/main/java/com/codegym/casestudym4/service/Image/ImageService.java
+++ src/main/java/com/codegym/casestudym4/service/Image/ImageService.java
@@ -1,4 +1,36 @@
 package com.codegym.casestudym4.service.Image;
 
-public class ImageService {
+import com.codegym.casestudym4.model.Image;
+import com.codegym.casestudym4.repository.IImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class ImageService implements IImageService{
+
+    @Autowired
+    private IImageRepository imageRepository;
+
+    @Override
+    public Iterable<Image> findAll() {
+        return imageRepository.findAll();
+    }
+
+    @Override
+    public Optional<Image> findById(Long id) {
+        return imageRepository.findById(id);
+    }
+
+    @Override
+    public Image save(Image image) {
+        return imageRepository.save(image);
+    }
+
+    @Override
+    public void delete(Long id) {
+        imageRepository.deleteById(id);
+    }
 }
